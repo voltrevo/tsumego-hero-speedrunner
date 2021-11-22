c233d30ff2ff5a3549a6514de95164642c86c216
Andrew Morris <voltrevo@gmail.com>
2021-11-22 17:03:31 +1100

    Fix shuffle

--- src/helpers/shuffle.ts
+++ src/helpers/shuffle.ts
@@ -2,7 +2,7 @@ export default function shuffle<T>(array: T[]) {
   const result = array.slice();
 
   for (let i = 0; i < result.length; i++) {
-    const j = Math.floor((result.length - i) * Math.random());
+    const j = i + Math.floor((result.length - i) * Math.random());
     [result[i], result[j]] = [result[j], result[i]];
   }
 
