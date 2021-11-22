df3a8531aae7eb86a64fd91d8c4dc1a27df68f54
Andrew Morris <voltrevo@gmail.com>
2021-11-22 14:23:48 +1100

    Basic set up

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+module.exports = {
+  extends: [
+    "airbnb-as-warnings",
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    quotes: ["warn", "double"],
+  },
+};
